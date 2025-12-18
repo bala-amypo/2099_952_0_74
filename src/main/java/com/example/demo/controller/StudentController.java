ab5fa4643a5cce8638ce1aa851139d0073508f6b
bala-amypo <bala@amypo.in>
2025-12-18 06:33:36 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -27,6 +27,6 @@ public class StudentController{
     }
     @DeleteMapping("/delete/{id}")
     public String delete(@PathVariable Long id){
-      return stdser.deleteStudent(id);
+      return stdser.deleteData(id);
     }
 }
--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -35,7 +35,7 @@ public class StudentServiceImpl implements StudentService{
        }
        @Override
        public String deleteData(Long id){
-       boolean status=stdrepo.existById(id);
+       boolean status=stdrepo.existsById(id);
        if(status){
        stdrepo.deleteById(id);
        return "Student Deleted Successfully";
